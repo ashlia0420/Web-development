cad8fef860cbd38991d4a273fd988e7f1cb58f06
ashlia0420 <ashliakbijumon0420@gmail.com>
2026-09-24 22:46:42 +0530

    Organising repos

---
+++ JavaScript/declare_intialize/script.js
@@ -0,0 +1,53 @@
+// // console.log("Goof Afternoon");
+// // let movie;
+// // console.log(movie);
+// // console.log("Dont watch moviws");
+// // console.log(theatre);
+// // var price;
+// // console.log(price);
+// // console.log("Center mall");
+
+
+// console.log("it takes 25 days to complete js");
+// let price=30000;
+// console.log("course fee: "+price);
+// var courseName;
+// console.log("CourseName");
+// console.log("kochi branch");
+// var duration;
+// console.log(duration);
+// console.log(trainer);
+// console.log("venkat");
+// console.log(courseName);
+
+
+// difference between var, let, const
+// VAR
+// var a;  //declaration possible
+// a=10; //initialisation possible
+// console.log(a);
+// a=50; //Re-initialisation possible
+// console.log(a);
+// var a; //re-declaration possible
+
+
+// LET
+// let a;  //declaration possible
+// a=100; //initialisation possible
+// console.log(a);
+// a=300; //Re-initialisation possible
+// console.log(a);
+// let a; //re-declaration is not possible
+
+
+// CONST
+// const a;  //declaration not possible
+
+// var price=10;
+
+
+let score=0;
+score=score+10;
+score=score+20;
+score=score+30;
+console.log("Final Score: ",score);
